90eb8ba995d3d0b69709dc413bcee12da5dd58bd
Mykyta Khliestov <khlestovnikita@gmail.com>
2013-08-21 18:47:05 +0300

    use login_required for /post

--- roomlrtest/roomlrtest/urls.py
+++ roomlrtest/roomlrtest/urls.py
@@ -1,13 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.views.generic import TemplateView
 
+from roomt.views import post_login
+
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'', include('social_auth.urls')),
     url(r'^$', TemplateView.as_view(template_name="pre.html")),
-    url(r'^post$', TemplateView.as_view(template_name="post.html")),
+    url(r'^post$', post_login, name='post_login'),
     url(r'^error$', TemplateView.as_view(template_name="error.html")),
 
     url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
--- roomlrtest/roomt/views.py
+++ roomlrtest/roomt/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render_to_response
+
+@login_required
+def post_login(request):
+    response = render_to_response('post.html', {'user': request.user})
+    return response
